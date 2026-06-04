5911f7c2baf5c1971c3827cbf550de476ed63793
nick-bennett <nbennett10@cnm.edu>
2026-06-04 15:32:02 -0600

    implemented basic Rank, Suit, and Card.Card

--- src/main/java/edu/cnm/deepdive/playingcards/model/Card.java
+++ src/main/java/edu/cnm/deepdive/playingcards/model/Card.java
@@ -1,47 +1,6 @@
 package edu.cnm.deepdive.playingcards.model;
 
-import java.util.Objects;
-
-public class Card implements Comparable<Card> {
-
-  private final Rank rank;
-  private final Suit suit;
-
-  public Card(Rank rank, Suit suit) {
-    this.rank = rank;
-    this.suit = suit;
-  }
-
-  public Rank rank() {
-    return rank;
-  }
-
-  public Suit suit() {
-    return suit;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(rank, suit);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    boolean equalityResult;
-    if (this == obj) {
-      equalityResult = true;
-    } else if (obj instanceof Card other) {
-      equalityResult = (this.rank == other.rank) && (this.suit == other.suit);
-    } else {
-      equalityResult = false;
-    }
-    return equalityResult;
-  }
-
-  @Override
-  public String toString() {
-    return rank + " " + suit;
-  }
+public record Card(Rank rank, Suit suit) implements Comparable<Card> {
 
   @Override
   public int compareTo(Card other) {
